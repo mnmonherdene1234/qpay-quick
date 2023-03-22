8d7fe39fac9040ea681a9cf244c2c0bd981cf627
mnmonherdene <mnmonherdene1234@gmail.com>
2023-03-22 14:54:16 +0800

    jsdoc error fixed

--- src/dto/qpay-enumerations.ts
+++ src/dto/qpay-enumerations.ts
@@ -9,47 +9,91 @@ export enum QPayEnvironment {
  * @enum {string}
  */
 export enum AccountBankCode {
-  /** @member {string} */
-  /** Default value*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Default value
+   */
   None = '',
 
-  /** @member {string} */
-  /** Мобифинанс*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Мобифинанс
+   */
   Mobifinance = '990000',
 
-  /** @member {string} */
-  /** Капитал банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Капитал банк
+   */
   Capitalbank = '020000',
 
-  /** @member {string} */
-  /** Худалдаа хөгжлийн банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Худалдаа хөгжлийн банк
+   */
   TDB = '040000',
 
-  /** @member {string} */
-  /** Хаан банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Хаан банк
+   */
   Khanbank = '050000',
 
-  /** @member {string} */
-  /** Голомт банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Голомт банк
+   */
   Golomtbank = '150000',
 
-  /** @member {string} */
-  /** Тээвэр хөгжлийн банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Тээвэр хөгжлийн банк
+   */
   Transbank = '190000',
 
-  /** @member {string} */
-  /** Капитрон банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Капитрон банк
+   */
   Capitronbank = '300000',
 
-  /** @member {string} */
-  /** Хас банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Хас банк
+   */
   Xacbank = '320000',
 
-  /** @member {string} */
-  /** Төрийн банк*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Төрийн банк
+   */
   Statebank = '340000',
 
-  /** @member {string} */
-  /** Төрийн сан*/
+  /**
+   * @member {string}
+   */
+  /**
+   * Төрийн сан
+   */
   Toriinsan = '900000',
 }
